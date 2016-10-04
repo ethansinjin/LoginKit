92537813ee3da35b1b468644ff29d06979d6c92d
Ethan Gill <egill@elinkdesign.com>
2016-10-04 08:34:59 -0400

    add initial framework project

---
+++ LoginKit/LoginKit.h
@@ -0,0 +1,19 @@
+//
+//  LoginKit.h
+//  LoginKit
+//
+//  Created by Ethan eLink on 10/4/16.
+//  Copyright © 2016 Ethan Gill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoginKit.
+FOUNDATION_EXPORT double LoginKitVersionNumber;
+
+//! Project version string for LoginKit.
+FOUNDATION_EXPORT const unsigned char LoginKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoginKit/PublicHeader.h>
+
+
