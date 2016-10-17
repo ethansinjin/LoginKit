2b1e2b356f418edd90cdff318ffc2d036b293626
Ethan Gill <egill@elinkdesign.com>
2016-10-17 13:11:28 -0400

    add LoginKitTest target for iOS

---
+++ LoginKitTest/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  LoginKitTest
+//
+//  Created by Ethan eLink on 10/17/16.
+//  Copyright © 2016 Ethan Gill. All rights reserved.
+//
+
+import UIKit
+import LoginKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func button1Pressed(_ sender: AnyObject) {
+        let loginViewController = LoginKitRootViewController(backgroundImage: #imageLiteral(resourceName: "background"),
+            logoImage: #imageLiteral(resourceName: "background"),
+            buttonBackgroundColor: #colorLiteral(red: 0.5948053598, green: 0.1391471922, blue: 0.164798677, alpha: 1),
+            buttonTextColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1),
+            buttonText: "Login") {
+            (username, password) in
+                self.dismiss(animated: true, completion: nil)
+            }
+        self.present(loginViewController, animated: true, completion: nil)
+    }
+
+}
+
